9cfa07fbf557e3a854bc9e2df0dd70a972d68dcd
samueldcabral <samueldcabral@gmail.com>
2018-12-30 21:08:18 -0300

    card finished

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -4,13 +4,15 @@ import TableWeather from './Table'
 
 export default class Search extends Component {
   state = {
-    weatherState: {}
+    weatherState: {},
+    bulkWeather: []
   }
 
   componentWillMount = () => {
     let API_KEY =  '6148faff9108a7d1f74a9a332767fbfd';
-    let URL = `http://api.openweathermap.org/data/2.5/weather?q=London&appid=${API_KEY}`
-   
+    let URL = `http://api.openweathermap.org/data/2.5/weather?q=London&appid=${API_KEY}&units=metric`
+    let bulk_URL = `http://api.openweathermap.org/data/2.5/forecast?q=London&appid=${API_KEY}&units=metric`
+
     axios.get(URL)
       .then(res => {
         let {visibility, main, wind, name,weather} = res.data
@@ -18,6 +20,26 @@ export default class Search extends Component {
         let weatherState = {'visibility': visibility, 'windSpeed': wind.speed, 'name': name, 'weather': weather[0].main, 'min': temp_min, 'max': temp_max, 'temp': temp, 'humidity': humidity}
         
         this.setState({weatherState})
+     })
+
+     axios.get(bulk_URL)
+     .then(res => {
+       let result = res.data.list.map(x => {
+          return {'Date': x.dt_txt, 'Day': new Date(x.dt_txt).getDay(), 'minTemp': x.main.temp_min, 'maxTemp': x.main.temp_max}
+       })
+       let index = result[0].Day
+       let resultTwo = result.filter(x => {
+         let hour = x.Date.split(' ')[1]
+
+         if(x.Day === index && hour.includes('09')){
+           index++
+           return true
+         }else{
+           return false
+         }
+       })
+       let bulkWeather = resultTwo
+       this.setState({bulkWeather})
      })
   }
   
@@ -27,7 +49,7 @@ export default class Search extends Component {
       <div className="tempo">
         <h1>Previsão do tempo</h1>
         <br></br>
-        <TableWeather weather={this.state.weatherState} />
+        <TableWeather weather={this.state.weatherState} bulkWeather={this.state.bulkWeather}/>
         <input type="text" className="input-box" placeholder="Insira aqui o nome da cidade" />
         <i className="fas fa-search search-icon"></i>
       </div>
--- src/Components/Table.js
+++ src/Components/Table.js
@@ -1,22 +1,30 @@
 import React from 'react'
 
-const TableWeather = ({ weather }) => {
-  console.log(`this is ${weather.name}`)
-  
+const TableWeather = ({ weather, bulkWeather }) => {
+  const days = (bulkWeather.map(x => {
+    return (
+      <th class="widthTh">{getDay[x.Day]}</th>
+    )
+  }))
+  const temps = (bulkWeather.map(x => {
+    return (
+      <td class="widthTd"><div class="gridA"><div class="leftA">{displayTemp(x.minTemp)}°</div><div class="rightA">{displayTemp(x.maxTemp)}°</div></div></td>
+    )
+  }))
   return (
     <div className="cardTempo grid">
       <h3 className="cardTempo__h3">{weather.name}</h3>
       <div className="cardTempo__X">X</div>
-      <h2 className="cardTempo__h2 text_left">{kelvinToC(weather.temp)}°C {weather.weather}</h2>
+      <h2 className="cardTempo__h2 text_left">{displayTemp(weather.temp)}°C {weather.weather}</h2>
       <section className="gridCard">
         <div className="flex minmax text_left gridarea">
           <i class="fa fa-arrow-down arrow" aria-hidden="true"></i>
-          <p className="min-max"><strong>{kelvinToC(weather.min)}°</strong></p>
+          <p className="min-max"><strong>{displayTemp(weather.min)}°</strong></p>
           <i class="fa fa-arrow-up arrow" aria-hidden="true"></i>
-          <p className="min-max "><strong>{kelvinToC(weather.max)}°</strong></p>
+          <p className="min-max "><strong>{displayTemp(weather.max)}°</strong></p>
         </div>
         <div className="sensacao text_left gridarea">
-          <p>Sensação <strong>{kelvinToC(weather.temp)}°</strong></p> 
+          <p>Sensação <strong>{displayTemp(weather.temp)}°</strong></p> 
         </div>
         <div className="vento text_left gridarea">
           <p>Vento <strong>{weather.windSpeed}km/h</strong></p> 
@@ -26,13 +34,29 @@ const TableWeather = ({ weather }) => {
         </div>
       </section>
       <hr></hr>
+      <table class="table">
+        <tr>
+         {days}
+        </tr>
+        <tr>
+          {temps}
+        </tr>
+      </table>
     </div>
   )
 }
 
-
-const kelvinToC = (temp) => {
-  let tempC = (temp - 273.15).toFixed(0);
+const getDay = {
+  0: 'Domingo',
+  1: 'Segunda',
+  2: 'Terça',
+  3: 'Quarta',
+  4: 'Quinta',
+  5: 'Sexta',
+  6: 'Sábado'
+}
+const displayTemp = (temp) => {
+  let tempC = Math.round(temp).toFixed(0)
   return tempC;
 }
 
